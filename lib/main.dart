b3e6e893f2ad1318c46e7201379326ffa6cb02ad
syalosovetskyi <43996021+syalosovetskyi@users.noreply.github.com>
2021-12-10 01:43:51 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future fetchData() async {
+  var _url = 'https://www.lesswrong.com/library';
+  _url = 'https://google.com';
+  var header = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+    "Access-Control-Allow-Origin, Origin, Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, locale",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+  };
+
+  final response = await http
+      .get(Uri.parse(_url), headers: header);
+
+  if (response.statusCode == 200) {
+    print("Response status: ${response.statusCode}");
+    // print("Response body: ${response.body}");
+
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return response.body;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future future_fetch;
+
+  @override
+  void initState() {
+    super.initState();
+    future_fetch = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder(
+            future: future_fetch,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.toString());
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
